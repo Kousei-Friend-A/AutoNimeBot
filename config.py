cd4c41c4eeeb77e41e3845bfa500064d97e4f787
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-04-02 12:21:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = 8143727
 API_HASH = "e2e9b22c6522465b62d8445840a526b1"
-BOT_TOKEN = "5383246595:AAF7UYG-baggRelLuAATKQ6qPpANq_qJP6Q"
+BOT_TOKEN = "5280448907:AAHnMAQVAFaaDNW9KbIPI6Kn7s8e3VJ6lc0"
 
 MONGO_DB_URI = "mongodb+srv://manudarkdevil911:manu1234@cluster0.alnwwhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
