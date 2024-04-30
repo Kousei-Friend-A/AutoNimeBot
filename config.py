f0c88378bba072391f0e9ae30d776fecbd2928e8
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-04-30 11:32:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = 8143727
 API_HASH = "e2e9b22c6522465b62d8445840a526b1"
 BOT_TOKEN = "5280448907:AAHnMAQVAFaaDNW9KbIPI6Kn7s8e3VJ6lc0"
 
-MONGO_DB_URI = "mongodb+srv://manudarkdevil911:manu1234@cluster0.alnwwhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI = "mongodb+srv://AiringAnime:portal123@cluster0.lbsfc9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 INDEX_ID = -1002023249945
 UPLOADS_ID = -1002094965664
